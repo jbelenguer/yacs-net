4717053ed38a0b8d742d74d7113c78cad04fd5b0
Jorge Belenguer <jorge.belenguer@uk.renaultsportracing.com>
2020-12-29 02:12:02 +0000

    Added nuget configurations and comments

--- Yacs/Exceptions/OfflineChannelException.cs
+++ Yacs/Exceptions/OfflineChannelException.cs
@@ -4,11 +4,21 @@ using System.Runtime.Serialization;
 
 namespace Yacs.Exceptions
 {
+    /// <summary>
+    /// Represents an error due to a <see cref="Channel"/> being offline.
+    /// </summary>
     [Serializable]
     public class OfflineChannelException : Exception
     {
+        /// <summary>
+        /// Gets the <see cref="EndPoint"/> that seems offline.
+        /// </summary>
         public EndPoint EndPoint { get; private set; }
 
+        /// <summary>
+        /// Creates a new <see cref="OfflineChannelException"/>.
+        /// </summary>
+        /// <param name="remoteEndpoint"></param>
         public OfflineChannelException(EndPoint remoteEndpoint) : base($"The endpoint {remoteEndpoint} is not registered as an online client.")
         {
             EndPoint = remoteEndpoint;
--- Yacs/Exceptions/ServerFailureException.cs
+++ Yacs/Exceptions/ServerFailureException.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace Yacs.Exceptions
 {
+    /// <summary>
+    /// Represents an error in the <see cref="Server"/>.
+    /// </summary>
     public class ServerFailureException : ApplicationException
     {
+        /// <summary>
+        /// Creates a new <see cref="ServerFailureException"/>.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
         public ServerFailureException(string message, Exception innerException) : base(message, innerException)
         {
         }
