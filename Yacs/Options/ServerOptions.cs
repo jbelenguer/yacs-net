7800967f9b287376388c975243007eb212f54576
Jorge Belenguer <jorge.belenguer@uk.renaultsportracing.com>
2020-12-29 02:12:02 +0000

    Added nuget configurations and comments

--- Yacs/Options/ServerOptions.cs
+++ Yacs/Options/ServerOptions.cs
@@ -16,7 +16,7 @@
         public int DiscoveryPort { get; set; }
 
         /// <summary>
-        /// Creates a new <see cref="ServerOptions" object with the default values./>
+        /// Creates a new <see cref="ServerOptions"/> object with the default values.
         /// </summary>
         public ServerOptions() : base()
         {
